5caca3df30632e024feacefed8713993224c9caa
saraswatpuneet <puneet.saraswat10074@gmail.com>
2022-01-29 10:19:01 -0600

    update chapter

--- handlers/chapter_create.go
+++ handlers/chapter_create.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -41,6 +42,49 @@ func ChapterCreate(ctx context.Context, courseID string, chapter *model.ChapterI
 		CourseID:    cassandraChapter.CourseID,
 		ModuleID:    chapter.ModuleID,
 		Sequence:    chapter.Sequence,
+	}
+	return &responseModel, nil
+}
+func UpdateChapter(ctx context.Context, chapter *model.ChapterInput) (*model.Chapter, error) {
+	log.Info("ChapterUpdate called")
+	if chapter.ID == nil {
+		return nil, fmt.Errorf("chapter not found")
+	}
+	cassandraChapter := coursez.Chapter{
+		ID: *chapter.ID,
+	}
+	// set course in cassandra
+	getQuery := global.CassSession.Session.Query(coursez.ChapterTable.Get()).BindStruct(cassandraChapter)
+	if err := getQuery.ExecRelease(); err != nil {
+		return nil, err
+	}
+	if chapter.Name != "" {
+		cassandraChapter.Name = chapter.Name
+	}
+	if chapter.Description != "" {
+		cassandraChapter.Description = chapter.Description
+	}
+	if chapter.Sequence != nil {
+		cassandraChapter.Sequence = *chapter.Sequence
+	}
+	if chapter.ModuleID != nil {
+		cassandraChapter.ModuleID = *chapter.ModuleID
+	}
+	cassandraChapter.UpdatedAt = time.Now().Unix()
+	updateQuery := global.CassSession.Session.Query(coursez.ChapterTable.Update()).BindStruct(cassandraChapter)
+	if err := updateQuery.ExecRelease(); err != nil {
+		return nil, err
+	}
+	created := strconv.FormatInt(cassandraChapter.CreatedAt, 10)
+	responseModel := model.Chapter{
+		ID:          &cassandraChapter.ID,
+		Name:        cassandraChapter.Name,
+		Description: cassandraChapter.Description,
+		CreatedAt:   &created,
+		UpdatedAt:   &created,
+		CourseID:    cassandraChapter.CourseID,
+		ModuleID:    &cassandraChapter.ModuleID,
+		Sequence:    &cassandraChapter.Sequence,
 	}
 	return &responseModel, nil
 }
