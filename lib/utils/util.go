5e818c5b90bfcb48643763bebf720895ac871c5b
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-02-17 20:16:04 -0600

    dont over parallelize

--- lib/utils/util.go
+++ lib/utils/util.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"context"
 	"io"
-	"sync"
 
 	"github.com/99designs/gqlgen/graphql"
 	log "github.com/sirupsen/logrus"
@@ -41,7 +40,6 @@ func init() {
 			// read the file in chunks and upload incrementally
 			// create chunks of 10mb
 			buf := make([]byte, 10*1024*1024)
-			var wg sync.WaitGroup
 			for {
 				n, err := req.File.File.Read(buf)
 				if err == io.EOF {
@@ -49,23 +47,14 @@ func init() {
 				}
 				if err != nil {
 					log.Errorf("Failed to read file: %v", err.Error())
-					ErrorQueue <- err
+					panic(err.Error())
 				}
-
-				// increment the WaitGroup counter for each chunk
-				wg.Add(1)
-				go func(chunk []byte) {
-					_, err = writer.Write(chunk)
-					if err != nil {
-						log.Errorf("Failed to upload file: %v", err.Error())
-						ErrorQueue <- err
-					}
-					wg.Done()
-				}(buf[:n])
+				_, err = writer.Write(buf[:n])
+				if err != nil {
+					log.Errorf("Failed to upload file: %v", err.Error())
+					panic(err.Error())
+				}
 			}
-
-			// wait for all the chunks to be uploaded before closing the writer
-			wg.Wait()
 			err = writer.Close()
 			if err != nil {
 				log.Errorf("Failed to close writer: %v", err.Error())
